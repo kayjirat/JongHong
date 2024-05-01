159659843bfe2624468279d166b5e7101cfbc8d9
opnxjm <onparin.jitm@mail.kmutt.ac.th>
2024-05-01 16:57:44 +0700

    Update authenChrome

--- jonghong/lib/pages/home_page.dart
+++ jonghong/lib/pages/home_page.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sign_in_button/sign_in_button.dart';
+import 'package:flutter/foundation.dart' as foundation;
+
+bool isWeb = identical(foundation.defaultTargetPlatform, TargetPlatform.fuchsia);
+bool isAndroid = identical(foundation.defaultTargetPlatform, TargetPlatform.android);
+bool isIOS = identical(foundation.defaultTargetPlatform, TargetPlatform.iOS);
+
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -64,12 +72,33 @@ class _HomepageState extends State<Homepage> {
   }
 
   void _handleGoogleSignIn(){
-    try{
-      GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
-      _auth.signInWithProvider(_googleAuthProvider);
-    }
-    catch(error){
-      print(error);
-    }
+    // try{
+    //   GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
+    //   _auth.signInWithPopup(_googleAuthProvider);
+      
+    // }
+    // catch(error){
+    //   print(error);
+    // }
+    if (isWeb || foundation.kIsWeb) {
+      try {
+        GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
+        _auth.signInWithPopup(googleAuthProvider);
+      } catch (error) {
+        print("Error signing in with Google: $error");
+      }
+    } else {
+      // Use provider-based login for mobile (Android/iOS)
+      try {
+        GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
+        _auth.signInWithProvider(googleAuthProvider);
+      } catch (error) {
+        print("Error signing in with Google: $error");
+      }
+    } 
+//else {
+//   // Handle other platforms (if applicable)
+// }
+
   }
 }
