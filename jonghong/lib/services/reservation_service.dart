13a57d7bdbfbfb3956e0e0d7312bd7c00b27f80b
opnxjm <onparin.jitm@mail.kmutt.ac.th>
2024-05-09 15:56:24 +0700

    EDIT reservation condition

--- jonghong/lib/services/reservation_service.dart
+++ jonghong/lib/services/reservation_service.dart
@@ -4,6 +4,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ReservationService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<String> timeSlot = [
+    '08.00-10.00',
+    '10.00-12.00',
+    '12.00-14.00',
+    '14.00-16.00',
+  ];
   Future<bool> isRoomAvailable(
       String roomId, DateTime reserveDate, String reserveTime) async {
     try {
@@ -82,6 +88,39 @@ class ReservationService {
       }
     } catch (e) {
       print('Error: $e');
+    }
+  }
+  Future <bool> findReservedTime(String roomId, DateTime date, String reserveTime) async {
+    try {
+      print(reserveTime);
+      QuerySnapshot reservationSnapshot = await _firestore
+          .collection('reservation')
+          .where('roomId', isEqualTo: roomId)
+          .where('reserveDate', isEqualTo: date.toString())
+          .where('reserveTime', isEqualTo: reserveTime)
+          .get();
+          print(reservationSnapshot.docs.isNotEmpty);
+      return reservationSnapshot.docs.isNotEmpty;
+    } catch (e) {
+      print('Error: $e');
+      return false;
+    }
+  }
+  Future<List<String>> notReservedTime(String roomId, DateTime date) async {
+    try {
+      QuerySnapshot reservationSnapshot = await _firestore
+          .collection('reservation')
+          .where('roomId', isEqualTo: roomId)
+          .where('reserveDate', isEqualTo: date.toString())
+          .get();
+      List<String> reservedTime = [];
+      for (var doc in reservationSnapshot.docs) {
+        reservedTime.add(doc.get('reserveTime'));
+      }
+      return timeSlot.where((element) => !reservedTime.contains(element)).toList();
+    } catch (e) {
+      print('Error: $e');
+      return [];
     }
   }
 }
