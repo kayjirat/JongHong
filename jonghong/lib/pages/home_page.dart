8bf7335aaa26a189fb6fa8c52cf87162b143364e
earaen <pavikaforlearning@gmail.com>
2024-05-10 22:22:19 +0700

    [EDIT] fix the login page overflow

--- jonghong/lib/pages/home_page.dart
+++ jonghong/lib/pages/home_page.dart
@@ -9,11 +9,12 @@ import 'package:flutter/foundation.dart' as foundation;
 import 'package:jonghong/services/firebase_service.dart';
 import 'package:jonghong/pages/room_list.dart';
 
-bool isWeb =
-    identical(foundation.defaultTargetPlatform, TargetPlatform.fuchsia);
-bool isAndroid =
-    identical(foundation.defaultTargetPlatform, TargetPlatform.android);
-bool isIOS = identical(foundation.defaultTargetPlatform, TargetPlatform.iOS);
+bool isWeb = identical(
+    foundation.defaultTargetPlatform, TargetPlatform.fuchsia);
+bool isAndroid = identical(
+    foundation.defaultTargetPlatform, TargetPlatform.android);
+bool isIOS =
+    identical(foundation.defaultTargetPlatform, TargetPlatform.iOS);
 
 typedef SignOutCallback = Future<void> Function();
 late final SignOutCallback signOutCallback;
@@ -41,8 +42,8 @@ class _HomepageState extends State<Homepage> {
   //   ));
   // }
 
-final GoogleSignIn googleSignIn = GoogleSignIn();
-final FirebaseAuth auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth auth = FirebaseAuth.instance;
 
 // Future<void> _handleGoogleSignIn() async {
 
@@ -56,14 +57,14 @@ final FirebaseAuth auth = FirebaseAuth.instance;
 //     final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
 //     if (googleUser == null) {
 //       print('Google Sign-In was cancelled or failed.');
-//       return; 
+//       return;
 //     }
 
 //     final GoogleSignInAuthentication? googleAuth = await googleUser.authentication;
 
 //     if (googleAuth == null) {
 //       print('Google authentication data is not available.');
-//       return; 
+//       return;
 //     }
 //     final FirebaseAuth auth = FirebaseAuth.instance;
 //     final AuthCredential credential = GoogleAuthProvider.credential(
@@ -72,7 +73,7 @@ final FirebaseAuth auth = FirebaseAuth.instance;
 //     );
 //       userCredential = await auth.signInWithCredential(credential);
 //     }
-    
+
 //     await FirebaseService().checkOrCreateUser(userCredential.user!);
 
 //     Navigator.push(
@@ -85,199 +86,218 @@ final FirebaseAuth auth = FirebaseAuth.instance;
 //     print('Error during Google Sign-In: $e');
 //   }
 // }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home Page',
       home: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(0xFFFF8F4C),
-                    Color(0xFFFE5B3D),
-                    Color(0xFFFE3231),
-                  ],
-                  stops: [0.0, 0.19, 1.0],
-                ),
-              ),
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          // Wrap SingleChildScrollView with Container
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFFFF8F4C),
+                Color(0xFFFE5B3D),
+                Color(0xFFFE3231),
+              ],
+              stops: [0.0, 0.19, 1.0],
             ),
-            //Logo+description+whitebox
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 65.0, left: 20.0),
-                    child: Image.asset(
-                      'assets/images/whiteLogo.png',
-                      width: 120,
-                      height: 122,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 17.0),
-                    child: Text(
-                      'JONG HONG',
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 35,
-                          color: Colors.white),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 7.0),
-                    child: Text(
-                      'S14 Room Reservation Application',
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 10,
-                          color: Colors.white),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 190.0),
-                    child: Container(
-                      width: double.infinity,
-                      height: 318.2,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30),
-                        ),
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            //pic+wording+button
-            Container(
-              margin: const EdgeInsets.only(top: 270),
-              child: Center(
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'assets/images/playPhone.png',
-                      width: 300,
-                      height: 300,
-                    ),
-                    const Text(
-                      'LET\'S GET START',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 27,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 13.0),
-                      child: Text(
-                        'Easier to make room reservation',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 11,
-                          color: Color(0xFF8D8D8D),
+          ),
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 65.0, left: 20.0),
+                        child: Image.asset(
+                          'assets/images/whiteLogo.png',
+                          width: 120,
+                          height: 122,
                         ),
                       ),
-                    ),
-
-                    //Button
-                    GestureDetector(
-                      onTap: () async {
-                        try {
-                          final auth = FirebaseAuth.instance;
-                          UserCredential userCredential;
-                          if (isWeb || kIsWeb) {
-                            final GoogleAuthProvider googleProvider = GoogleAuthProvider();
-                            userCredential = await auth.signInWithPopup(googleProvider);
-                          } else {
-                            final GoogleSignIn googleSignIn = GoogleSignIn();
-                            final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-                            if (googleUser == null) {
-                              print('Google Sign-In was cancelled or failed.');
-                              return;
-                            }
-
-                            final GoogleSignInAuthentication googleAuth =
-                                await googleUser.authentication;
-
-                            if (googleAuth == null) {
-                              print('Google authentication data is not available.');
-                              return;
-                            }
-                            final AuthCredential credential = GoogleAuthProvider.credential(
-                              idToken: googleAuth.idToken,
-                              accessToken: googleAuth.accessToken,
-                            );
-                            userCredential = await auth.signInWithCredential(credential);
-                          }
-
-                          await FirebaseService().checkOrCreateUser(userCredential.user!);
-
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => RoomListPage(user: userCredential.user!),
-                            ),
-                          );
-                        } catch (e) {
-                          print('Error during Google Sign-In: $e');
-                        }
-                      },
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: const LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Color(0xFFFF8F4C),
-                              Color(0xFFFE5B3D),
-                              Color(0xFFFE3231),
-                            ],
+                      const Padding(
+                        padding: EdgeInsets.only(top: 17.0),
+                        child: Text(
+                          'JONG HONG',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35,
+                            color: Colors.white,
                           ),
                         ),
-                        child: const Center(
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 7.0),
+                        child: Text(
+                          'S14 Room Reservation Application',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w400,
+                            fontSize: 10,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 190.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: 318.2,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            ),
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 270),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/playPhone.png',
+                          width: 300,
+                          height: 300,
+                        ),
+                        const Text(
+                          'LET\'S GET START',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 27,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 13.0),
                           child: Text(
-                            'Login',
+                            'Easier to make room reservation',
                             style: TextStyle(
                               fontFamily: 'Poppins',
                               fontWeight: FontWeight.w500,
-                              fontSize: 14,
-                              color: Colors.white,
+                              fontSize: 11,
+                              color: Color(0xFF8D8D8D),
                             ),
                           ),
                         ),
-                      ),
+                        GestureDetector(
+                          onTap: () async {
+                            try {
+                              final auth = FirebaseAuth.instance;
+                              UserCredential userCredential;
+                              if (isWeb || kIsWeb) {
+                                final GoogleAuthProvider
+                                    googleProvider =
+                                    GoogleAuthProvider();
+                                userCredential = await auth
+                                    .signInWithPopup(googleProvider);
+                              } else {
+                                final GoogleSignIn googleSignIn =
+                                    GoogleSignIn();
+                                final GoogleSignInAccount?
+                                    googleUser =
+                                    await googleSignIn.signIn();
+                                if (googleUser == null) {
+                                  print(
+                                      'Google Sign-In was cancelled or failed.');
+                                  return;
+                                }
+
+                                final GoogleSignInAuthentication
+                                    googleAuth =
+                                    await googleUser.authentication;
+
+                                if (googleAuth == null) {
+                                  print(
+                                      'Google authentication data is not available.');
+                                  return;
+                                }
+                                final AuthCredential credential =
+                                    GoogleAuthProvider.credential(
+                                  idToken: googleAuth.idToken,
+                                  accessToken: googleAuth.accessToken,
+                                );
+                                userCredential = await auth
+                                    .signInWithCredential(credential);
+                              }
+
+                              await FirebaseService()
+                                  .checkOrCreateUser(
+                                      userCredential.user!);
+
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => RoomListPage(
+                                      user: userCredential.user!),
+                                ),
+                              );
+                            } catch (e) {
+                              print(
+                                  'Error during Google Sign-In: $e');
+                            }
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 30.0),
+                            child: Container(
+                              width: double.infinity,
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 10),
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 40, vertical: 20),
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(10),
+                                gradient: const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Color(0xFFFF8F4C),
+                                    Color(0xFFFE5B3D),
+                                    Color(0xFFFE3231),
+                                  ],
+                                ),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        //Button
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
-  
   }
 }
-
-
-
