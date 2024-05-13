4d19ba0f2691d9859f2a9316a68dde7b57fceea4
Dxchmxww <mwt7475@gmail.com>
2024-05-14 01:43:45 +0700

    restrict email

--- jonghong/lib/pages/home_page.dart
+++ jonghong/lib/pages/home_page.dart
@@ -2,9 +2,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/foundation.dart' as foundation;
-import 'package:jonghong/main.dart';
+import 'package:jonghong/pages/room_list.dart';
 import 'package:jonghong/services/firebase_service.dart';
 
 bool isWeb = identical(
@@ -138,93 +139,95 @@ class _HomepageState extends State<Homepage> {
                             ),
                           ),
                         ),
-                        GestureDetector(
-                          onTap: () async {
-                            try {
-                              final auth = FirebaseAuth.instance;
-                              UserCredential userCredential;
-                              if (isWeb || kIsWeb) {
-                                final GoogleAuthProvider
-                                    googleProvider =
-                                    GoogleAuthProvider();
-                                userCredential = await auth
-                                    .signInWithPopup(googleProvider);
-                              } else {
-                                final GoogleSignIn googleSignIn =
-                                    GoogleSignIn();
-                                final GoogleSignInAccount?
-                                    googleUser =
-                                    await googleSignIn.signIn();
-                                if (googleUser == null) {
-                                  print(
-                                      'Google Sign-In was cancelled or failed.');
-                                  return;
-                                }
+                        
+                    GestureDetector(
+                      onTap: () async {
+                        try {
+                          final auth = FirebaseAuth.instance;
+                          UserCredential userCredential;
 
-                                final GoogleSignInAuthentication
-                                    googleAuth =
-                                    await googleUser.authentication;
-
-                                final AuthCredential credential =
-                                    GoogleAuthProvider.credential(
-                                  idToken: googleAuth.idToken,
-                                  accessToken: googleAuth.accessToken,
-                                );
-                                userCredential = await auth
-                                    .signInWithCredential(credential);
-                              }
-
-                              await FirebaseService()
-                                  .checkOrCreateUser(
-                                      userCredential.user!);
-
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const BottomNavigation(),
-                                ),
-                              );
-                            } catch (e) {
-                              print(
-                                  'Error during Google Sign-In: $e');
+                          if (isWeb || kIsWeb) {
+                            final GoogleAuthProvider googleProvider = GoogleAuthProvider();
+                            userCredential = await auth.signInWithPopup(googleProvider);
+                          } else {
+                            final GoogleSignIn googleSignIn = GoogleSignIn();
+                            final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+                            if (googleUser == null) {
+                              print('Google Sign-In was cancelled or failed.');
+                              return;
                             }
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 20.0),
-                            child: Container(
-                              width: double.infinity,
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10),
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 40, vertical: 20),
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.circular(10),
-                                gradient: const LinearGradient(
-                                  begin: Alignment.topCenter,
-                                  end: Alignment.bottomCenter,
-                                  colors: [
-                                    Color(0xFFFF8F4C),
-                                    Color(0xFFFE5B3D),
-                                    Color(0xFFFE3231),
-                                  ],
-                                ),
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 14,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
+
+                            final GoogleSignInAuthentication googleAuth =
+                                await googleUser.authentication;
+
+                            if (googleAuth == null) {
+                              print('Google authentication data is not available.');
+                              return;
+                            }
+
+                            final AuthCredential credential = GoogleAuthProvider.credential(
+                              idToken: googleAuth.idToken,
+                              accessToken: googleAuth.accessToken,
+                            );
+
+                            // Email pattern validation
+                            final emailPattern = RegExp(r'^[a-zA-Z0-9_.+-]+@[mail.kmutt.ac.th]');
+                            if (!emailPattern.hasMatch(googleUser.email)) {
+                              Fluttertoast.showToast(
+                                msg: 'Invalid email format. Please use @mail.kmutt.ac.th email address.',
+                                gravity: ToastGravity.CENTER,
+                                backgroundColor: Color.fromARGB(255, 255, 149, 149),
+                                textColor: Colors.white,
+                                
+                              );
+                              return;
+                            }
+
+                            userCredential = await auth.signInWithCredential(credential);
+                          }
+
+                          await FirebaseService().checkOrCreateUser(userCredential.user!);
+
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RoomListPage(user: userCredential.user!),
+                            ),
+                          );
+                        } catch (e) {
+                          print('Error during Google Sign-In: $e');
+                        }
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: const LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color(0xFFFF8F4C),
+                              Color(0xFFFE5B3D),
+                              Color(0xFFFE3231),
+                            ],
+                          ),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                        //Button
+                      ),
+                    ),
+
                       ],
                     ),
                   ),
